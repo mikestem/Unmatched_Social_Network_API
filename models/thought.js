5c69854e706fcd1c11682f6597699a5b69517bec
mikestem <michael.a.stem@gmail.com>
2023-04-05 13:45:56 -0400

    added models

---
+++ models/thought.js
@@ -0,0 +1,65 @@
+const { Schema, model, types } = require ("mongoose");
+const dateFormat = require("../utils/date");
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model ("Thought", thoughtSchema);
+
+module.exports = Thought;
