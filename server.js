d386ce982e558fbb05baeef9c063b1980eaabb4a
mikestem <michael.a.stem@gmail.com>
2023-04-05 13:54:41 -0400

    updated server.js

--- server.js
+++ server.js
@@ -5,4 +5,16 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use(require("./routes"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/social-network",
+    {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+);
+
+mongoose.set("debug", true);
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
